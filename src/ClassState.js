8674584d3061aea60d983dd5a9e590435487a23e
wildchamo <wildchamo@gmail.com>
2022-12-21 20:17:28 -0500

    base de los componentes sin estilos

---
+++ src/ClassState.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class ClassState extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Eliminar ClassState</h2>
+        <p>Escribe el código de seguridad</p>
+        <input placeholder="Código de seguridad"/>
+        <button>Comprobar</button>
+      </div>
+    );
+  }
+}
+
+export { ClassState };
---
+++ src/UseState.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function UseState() {
+  return (
+    <div>
+      <h2>Eliminar UseState</h2>
+      <p>Escribe el código de seguridad</p>
+      <input placeholder="Código de seguridad" />
+      <button>Comprobar</button>
+    </div>
+  );
+}
+
+export { UseState };
