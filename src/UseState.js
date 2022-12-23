5b97172542bd269ec0c405d26791af6ebf5a29fb
wildchamo <wildchamo@gmail.com>
2022-12-22 20:17:29 -0500

    uso de useState con function components

--- src/UseState.js
+++ src/UseState.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function UseState({name}) {
+function UseState({ name }) {
+  const [error, setError] = React.useState(true);
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Escribe el código de seguridad</p>
+
+      {error && <p>Hubo un error, papalindo</p>}
       <input placeholder="Código de seguridad" />
-      <button>Comprobar</button>
+      <button onClick={() => setError(!error)}>Comprobar</button>
     </div>
   );
 }
