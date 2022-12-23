80d212252d0bb75cb8d196e8988b5bec6fae507e
wildchamo <wildchamo@gmail.com>
2022-12-23 12:10:44 -0500

    efectos con useEfect

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,13 +1,22 @@
 import React from "react";
 
 class ClassState extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: false,
+    };
+  }
   render() {
     return (
       <div>
         <h2>Eliminar {this.props.name}</h2>
         <p>Escribe el código de seguridad</p>
+        {this.state.error && <p>Hubo un error, papalindo</p>}
         <input placeholder="Código de seguridad" />
-        <button>Comprobar</button>
+        <button onClick={() => this.setState({ error: !this.state.error })}>
+          Comprobar
+        </button>
       </div>
     );
   }
--- src/UseState.js
+++ src/UseState.js
@@ -2,14 +2,33 @@ import React from "react";
 
 function UseState({ name }) {
   const [error, setError] = React.useState(true);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    console.log("empezando el efecto!");
+
+    if (loading) {
+      setTimeout(() => {
+        console.log("Haciendo validación!");
+
+        setLoading(false);
+
+        console.log("Terminando la  validación!");
+      }, 2000);
+    }
+
+    console.log("terminando el efecto!");
+  }, [loading]);
+
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Escribe el código de seguridad</p>
 
       {error && <p>Hubo un error, papalindo</p>}
+      {loading && <p>Cargando, papalindo</p>}
       <input placeholder="Código de seguridad" />
-      <button onClick={() => setError(!error)}>Comprobar</button>
+      <button onClick={() => setLoading(true)}>Comprobar</button>
     </div>
   );
 }
