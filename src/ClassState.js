7146c956c4cae2ad1af68b59ad1ba35e1893726a
wildchamo <wildchamo@gmail.com>
2022-12-24 15:15:55 -0500

    estados compuestos con class states

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Loading } from "./Loading";
 
+const SECURITY_CODE = "paradigma";
+
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      value: "",
       error: false,
       loading: false,
     };
@@ -22,30 +25,41 @@ class ClassState extends React.Component {
     console.log("componentDidMount");
   }
 
-  componentDidUpdate(){
-    console.log("actualización papá")
+  componentDidUpdate() {
+    console.log("actualización papá");
 
     if (this.state.loading) {
       setTimeout(() => {
         console.log("Haciendo validación!");
 
-        this.setState({loading:false});
-
+        if (SECURITY_CODE === this.state.value) {
+          this.setState({ error: false, loading: false });
+        } else {
+          this.setState({ error: true, loading: false });
+        }
         console.log("Terminando la  validación!");
       }, 2000);
     }
   }
 
   render() {
+    const { error, loading, value } = this.state;
+
     return (
       <div>
         <h2>Eliminar {this.props.name}</h2>
         <p>Escribe el código de seguridad</p>
-        {this.state.error && <p>Hubo un error, papalindo</p>}
-        {this.state.loading && <Loading/>}
-        <input placeholder="Código de seguridad" />
+        {(this.state.error && !this.state.loading) && <p>Hubo un error, papalindo</p>}
+        {this.state.loading && <Loading />}
+        <input
+          value={value}
+          onChange={(event) => {
+            this.setState({ value: event.target.value });
+          }}
+          placeholder="Código de seguridad"
+        />
         {/* <button onClick={() => this.setState({ error: !this.state.error })}> */}
-        <button onClick={() => this.setState({ loading:true })}>
+        <button onClick={() => this.setState({ loading: true })}>
           Comprobar
         </button>
       </div>
