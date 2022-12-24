a307f0074306652c1fb609adbda2e43cd6ef98fb
wildchamo <wildchamo@gmail.com>
2022-12-24 15:33:51 -0500

    uso de estados compuestos en react

--- src/UseState.js
+++ src/UseState.js
@@ -3,51 +3,74 @@ import React from "react";
 const SECURITY_CODE = "paradigma";
 
 function UseState({ name }) {
-  const [value, setValue] = React.useState("");
-  const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(false);
-  console.log(value);
+  const [state, setState] = React.useState({
+    value: "",
+    error: false,
+    loading: false,
+  });
+
+  console.log(state);
 
   React.useEffect(() => {
     console.log("empezando el efecto!");
 
-    if (loading) {
+    if (state.loading) {
       setTimeout(() => {
         console.log("Haciendo validación!");
-        if (value === SECURITY_CODE) {
-          // setError(true);
+        if (state.value === SECURITY_CODE) {
+          setState({
+            ...state,
+            error: false,
+            loading: false,
+          });
         } else {
-          setError(true);
+          setState({
+            ...state,
+            error: true,
+            loading: false,
+          });
         }
-        setLoading(false);
 
         console.log("Terminando la  validación!");
       }, 2000);
     }
 
     console.log("terminando el efecto!");
-  }, [loading]);
+  }, [state.loading]);
 
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Escribe el código de seguridad</p>
 
-      {/* {(error && !loading) && <p>Hubo un error, papalindo</p>} */}
-      {error && <p>Hubo un error, papalindo</p>}
-      {loading && <p>Cargando, papalindo</p>}
+      {state.error && !state.loading && <p>Hubo un error, papalindo</p>}
+      {/* {state.error && <p>Hubo un error, papalindo</p>} */}
+      {state.loading && <p>Cargando, papalindo</p>}
       <input
-        value={value}
+        value={state.value}
         onChange={(event) => {
-          setValue(event.target.value);
+          setState({
+            ...state,
+
+            value: event.target.value,
+          });
           // setError(false) se llama muchas veces al cambio de estado!
         }}
         placeholder="Código de seguridad"
       />
       <button
         onClick={() => {
-          setError(false); // Es correcto
-          setLoading(true);
+          setState({
+            ...state,
+
+            error: false,
+          });
+          //  setError(false); // Es correcto
+          setState({
+            ...state,
+
+            loading: true,
+          });
         }}
       >
         Comprobar
